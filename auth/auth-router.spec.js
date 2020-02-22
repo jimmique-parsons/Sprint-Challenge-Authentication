0304c77fc19ca07340cf56c012dc0dba146c71a8
Jimmique <jimmique88@gmail.com>
2020-02-21 20:04:11 -0800

    Corrected tests and addedauthentication middleware

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -1,42 +1,53 @@
 const router = require('./auth-router.js');
+const server = require('./../api/server.js');
 const db = require('./../database/dbConfig.js');
 const request = require('supertest');
 
 describe('POST /register', () => {
     it('should return the newly registered user', async (done) => {
-        request(router)
-            .post('/register')
-            .send({ username: 'jimmique', password: 'password' })
+        request(server)
+            .post('/api/auth/register')
+            .send({ username: 'newuser', password: 'password' })
             .set('Accept', 'application/json')
-            .expect(201, {
-                username: 'jimmique'
-            }, done);
+            .expect(201)
+            .end((err, res) => {
+                if (err) return done(err);
+                done();
+            });
     });
 
-    it('should return a 500 error', async (done) => {
-        request(router)
-            .post('/register')
+    it('should return a 500 error', (done) => {
+        request(server)
+            .post('/api/auth/register')
             .send({ username: 'emanresu' })
             .set('Accept', 'application/json')
-            .expect(500, done);
+            .expect(500)
+            .end((err, res) => {
+                if (err) return done(err);
+                done();
+            });
     });
 });
 
 describe('POST /login', () => {
-    it('should return a 200 status', async (done) => {
-        request(router)
-            .post('/login')
+    it('should return a 200 status', (done) => {
+        request(server)
+            .post('/api/auth/login')
             .send({ username: 'jimmique', password: 'password' })
             .set('Accept', 'application/json')
             .expect(200, done);
     });
 
-    it('should return a 401 error', async (done) => {
-        request(router)
-            .post('/login')
+    it('should return a 401 error', (done) => {
+        request(server)
+            .post('/api/auth/login')
             .send({ username: 'jimmique', password: 'drowssap' })
             .set('Accept', 'application/json')
-            .expect(401, done);
+            .expect(401)
+            .end((err, res) => {
+                if (err) return done(err);
+                done();
+            });
     });
 
 });
